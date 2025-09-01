eea4ff08611158a79c072c7ae016e56e9c427eda
Tomarun029831 <tomarun029831@gmail.com>
2025-09-01 16:25:00 +0900

    Initial Commit

---
+++ GeoFencingTest/GeoFencingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoFencingTestApp.swift
+//  GeoFencingTest
+//
+//  Created by Tomarun on 2025/09/01.
+//
+
+import SwiftUI
+
+@main
+struct GeoFencingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeoFencingTestTests/GeoFencingTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  GeoFencingTestTests.swift
+//  GeoFencingTestTests
+//
+//  Created by Tomarun on 2025/09/01.
+//
+
+import Testing
+
+struct GeoFencingTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
